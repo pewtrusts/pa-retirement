6f4374215c98abd912658adb05a5a0c97aa6e5d4
John Osterman <josterman@pewtrusts.org>
2020-08-10 21:46:39 -0400

    WIP map

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -11,16 +11,62 @@ if ( module.hot ){
   //  module.hot.accept('./styles.scss');
 }
 
+const fields  = ['liability', 'd_ratio', 'required'];
 const container = document.querySelector('#pa-map-container');
+const scale = d3.scaleLinear().range([0,1]);
+//var colors = ['#fff', '#229DC6','#153164'];
+var colors = ['#fff', '#296EC3','#153164'];
+const extents = {};
 
 export default function initMap({data}){
-    var svg = d3.select(container)
+    // mutates extents
+    fields.reduce(function(acc,cur){ 
+        extents[cur] = d3.extent(data, d => d[cur]);
+    },extents);
+    
+    var svgContainer = d3.select(container)
         .selectAll('div.js-svg-container')
         .data([data]);
 
     {
-        let entering = svg.enter()
-            .append('div.js-svg-container')
+        // only in dev or during prerender will there be an entering container
+        let entering = svgContainer.enter()
+            .append('div')
+            .attr('class','js-svg-container')
             .html(mapSVG);
+
+        svgContainer = svgContainer.merge(entering);
     }
+
+    var svg = svgContainer.select('svg');
+    var counties = svg.selectAll('path').data(d => d, function(_d){
+        return _d ? slugger(_d.county) : this.getAttribute('data-county');
+    });
+    // in all cases, dev, prerender, page load, the paths will already be part of the svg
+    //counties
+
+    counties.each(update.bind(undefined, fields[0]));
+
+    var labels = d3.selectAll('div.g-Layer_1');
+    console.log(labels);
+    labels.data(data, function(_d){
+        console.log(_d);
+        return _d ? slugger(_d.county) : slugger(this.getAttribute('data-key'));
+    });
+
+    labels.each(updateLabels.bind(undefined, fields[0]))
+}
+function update(field,d,i,array){
+    var county = d3.select(array[i]);
+    scale.domain(extents[field]);
+    county
+        .attr('fill', d => {
+            var interp = d3.piecewise(d3.interpolateRgb, colors);
+            return interp(scale(d[field]));
+    });
+}
+function updateLabels(field,d,i,array){
+    var label = d3.select(array[i]);
+    label
+        .style('color', d => scale(d[field]) < 0.5 ? '#000' : '#fff');
 }
--- src/d3-importer.js
+++ src/d3-importer.js
@@ -1,15 +1,20 @@
-//import { ascending, descending, deviation, extent, max, mean, median, min, range, sum, variance  } from 'd3-array';
+import { extent } from 'd3-array';
 //import { axisBottom, axisLeft } from 'd3-axis';
 //import { entries, nest}         from 'd3-collection';
 import { format, formatLocale } from 'd3-format';
+import { interpolateRgb, interpolateRgbBasis, piecewise }       from 'd3-interpolate';
 //import { path }                 from 'd3-path';
 import { scaleLinear }          from 'd3-scale';
 import { select, selectAll }    from 'd3-selection';
 import tip                      from 'd3-tip';
 // TO DO: IMPORT ONLY WHAT'S NEEDED
 export default {
+    extent,
     format,
     formatLocale,
+    interpolateRgb,
+    interpolateRgbBasis,
+    piecewise,
     scaleLinear,
     select,
     selectAll,
